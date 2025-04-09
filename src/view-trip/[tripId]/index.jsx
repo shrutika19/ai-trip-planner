d0221404617b35a32af766e3cea049b6dce883b6
Shrutika19 <jaiswalshrutika19@gmail.com>
2025-04-09 22:00:31 +0530

    fetched data from firestore

--- src/view-trip/[tripId]/index.jsx
+++ src/view-trip/[tripId]/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { db } from '@/service/firebaseConfig';
+import { doc, getDoc } from 'firebase/firestore';
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { toast } from 'sonner';
 
 function ViewTrip() {
-  return (
-    <div>index</div>
-  )
+
+    const { tripId } = useParams();
+
+    useEffect(() => {
+        getTripData();
+    }, [tripId])
+
+    const getTripData = async () => {
+        const docRef = doc(db, 'Aitrips', tripId);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            console.log("Document", docSnap.data())
+        } else {
+            console.log("No such document");
+            toast("No trip found!");
+        }
+    }
+
+    return (
+        <div>{tripId}</div>
+    )
 }
 
-export default ViewTrip;
+export default ViewTrip
