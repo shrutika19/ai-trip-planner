a720c313f9c5ff67095d4b6a3f50209fd01bd298
Shrutika19 <jaiswalshrutika19@gmail.com>
2025-04-24 21:19:43 +0530

    fetched images from google photo api in place section

--- src/view-trip/components/PlaceCard.jsx
+++ src/view-trip/components/PlaceCard.jsx
@@ -1,14 +1,36 @@
 import { Button } from '@/components/ui/button';
-import React from 'react'
+import { PHOTO_REF_URL } from '@/constants/options';
+import { GetPlaceDetails } from '@/service/GlobalApi';
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 // import { FaMapLocation } from "react-icons/fa6";
 
 
 function PlaceCard({ place }) {
+    const [photoUrl, setPhotoUrl] = useState();
+
+    useEffect(() => {
+        place && GetPlacePhoto();
+    }, [place])
+
+    const GetPlacePhoto = async () => {
+        const data = {
+            textQuery: place?.name
+        };
+
+        try {
+            const res = await GetPlaceDetails(data);
+            // Do something with res.data if needed
+            const PhotoUrl = PHOTO_REF_URL.replace('{NAME}', res.data.places[0].photos[3].name);
+            setPhotoUrl(PhotoUrl)
+        } catch (error) {
+            console.error("Place API Error:", error); // Full AxiosError with .response, .message, etc.
+        }
+    };
     return (
         <Link to={'https://www.google.com/maps/search/?api=1&query=' + place?.name} target='_blank'>
             <div className='border rounded-xl p-3 mt-2 flex gap-5 hover:scale-105 transition-all cursor-pointer hover:shadow-md'>
-                <img src='/placeholder.jpg' className='w-[130px] h-[130px] rounded-xl' />
+                <img src={photoUrl} className='w-[130px] h-[130px] rounded-xl object-cover' />
                 <div>
                     <h2 className='font-bold text-lg'>{place?.name}</h2>
                     <p className='text-sm text-gray-500'>{place?.description}</p>
